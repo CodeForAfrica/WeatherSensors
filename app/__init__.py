0b8be2a72a019f1c5121533c0ca1740cf2db8b98
mtuchi <mtuchidev@gmail.com>
2018-04-24 15:31:22 +0300

    Remove dead code
    Added google-maps-api-key,
    Installed pylint

--- app/__init__.py
+++ app/__init__.py
@@ -2,14 +2,15 @@
 from flask import Flask
 from flask_googlemaps import GoogleMaps
 import config
+import os
 # Initialize the app
 app = Flask(__name__, instance_relative_config=True)
 
 # Load the config file
 # app.config.from_object('config')
-# app.config.from_object('settings')
+# app.logger.info(os.getenv("MAP_WIDGETS"))
 
-GoogleMaps(app)
+GoogleMaps(app,key=os.getenv("GOOGLEMAPS_KEY"))
 
 # Load the views
 from app import views
